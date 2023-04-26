ccbde539f6d905f63dc9b729c8a2e0b61653846a
Thony11 <thonyceli11@gmail.com>
2023-04-26 14:02:31 -0500

    Cambios

--- ManejoClases/src/manejoclases03/Ejecutalble.java
+++ ManejoClases/src/manejoclases03/Ejecutalble.java
@@ -5,10 +5,34 @@
  */
 package manejoclases03;
 
+import java.util.Scanner;
+
 /**
  *
  * @author LENOVO
  */
 public class Ejecutalble {
-    
+    public static void main(String[] args){
+        Hospital miHospital = new Hospital();
+        Scanner entrada = new Scanner(System.in);
+        
+        String name;
+        int camas;
+        double saldo;
+        
+        System.out.println("Ingrese el nombre del hospital:");
+        name= entrada.next();
+        System.out.println("Ingrese el numero de camas del hospital:");
+        camas= entrada.nextInt();
+        System.out.println("Ingrese el presupuesto del hospital:");
+        saldo= entrada.nextDouble();
+        
+        miHospital.establecerNombre(name);
+        miHospital.establecerNumeroCamas(camas);
+        miHospital.establecerPresupuesto(saldo); 
+        
+        System.out.printf("%s - %d - %.2f\n", miHospital.obtenerNombre(),
+                miHospital.obtenerNumeroCamas(), 
+                miHospital.obtenerPresupuesto());
+    }
 }
